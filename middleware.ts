5b8e04d74fdf00e20070ae4e23bd428e9a071af0
elias <eliaschen.dev@gmail.com>
2025-10-21 22:02:04 +0800

    fix: temp fix for can't signin

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getUserInfo } from "./components/utils/getUserInfo";
+export { auth as middleware } from "@/auth";
 
-export async function middleware(request: NextRequest) {
-  const exportOnly = ["/dashboard", "/tasks/new"];
-  if (exportOnly.includes(request.nextUrl.pathname)) {
-    const user = await getUserInfo();
-    if (user.role !== "expert") {
-      return NextResponse.redirect(new URL("/unauthorized", request.url));
-    }
-  }
-  return NextResponse.next();
-}
+// export async function middleware(request: NextRequest) {
+//   const exportOnly = ["/dashboard", "/tasks/new"];
+//   if (exportOnly.includes(request.nextUrl.pathname)) {
+//     const user = await getUserInfo();
+//     if (user.role !== "expert") {
+//       return NextResponse.redirect(new URL("/unauthorized", request.url));
+//     }
+//   }
+//   return NextResponse.next();
+// }
